9cd23d0d3e00714e9ba08e9c5ffcd59e50bda0e5
cl519 <charles86519a@gmail.com>
2019-11-04 19:43:50 -0800

    Profile has button directs to google map

--- app/src/main/java/com/example/beanleaf/GotoProfile.java
+++ app/src/main/java/com/example/beanleaf/GotoProfile.java
@@ -1,4 +1,34 @@
 package com.example.beanleaf;
 
-class GotoProfile {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class GotoProfile extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.profile_layout);
+
+        //setContentView(R.layout.);
+
+        /*
+        Intent activityThatCalled = getIntent();
+        String previousActivity = activityThatCalled.getExtras().getString("callingActivity");
+        */
+
+
+    }
+
+    public void OpenMap(View view) {
+        Intent MapsActivityIntent = new Intent(this, MapsActivity.class);
+        //final int result = 1;
+
+        //GotoProfileIntent.putExtra("callingActivity", "MainActivity");
+        startActivity(MapsActivityIntent);
+//        startActivityForResult if you want to retrieve data back with result id
+    }
 }
